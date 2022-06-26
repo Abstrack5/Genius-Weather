d64ccf4d46de912c7161a403a0bcd2d2b40f548e
Armon Ahmadi <abstrack@Armons-MacBook-Air.local>
2022-06-26 01:29:01 -0400

    started js functions to call api and start searching city input

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,3 @@
-// var apiKey = "844421298d794574c100e3409cee0499"
-// var geoCall = `https://api.openweathermap.org/data/2.5/
 
 var searchCityForm = document.querySelector(".searchCityForm");
 var searchCityInput = document.querySelector("#city");
@@ -9,3 +7,35 @@ var searchedCity = document.querySelector(".searchedCity");
 
 var fiveDayForecastInfo = document.querySelector(".fiveDayForecastInfo");
 var fiveDayForecastInfo = document.querySelector(".fiveDayForecastInfo");
+
+var geoCall = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+var apiKey = "844421298d794574c100e3409cee0499"
+
+var cityInputName = searchCityInput.value.trim();
+var cityArr = [];
+
+
+var geoCallFetch = function() {
+    fetch(geoCall)
+    .then(function(response) {
+        response.json().then(function(data){
+            console.log(data);
+        });
+    });
+};
+
+
+var displayWeather = function(event){
+    event.preventDefault();
+    if(cityInputName){
+        geoCallFetch(cityInputName)
+    } else {
+        alert("Please enter a city name");
+    }
+};
+
+
+
+
+
+searchCityForm.addEventListener("submit", displayWeather)
