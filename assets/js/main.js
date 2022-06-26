a47f74833315bd2119d241d9ad9342435684d647
Armon Ahmadi <abstrack@Armons-MacBook-Air.local>
2022-06-25 23:53:55 -0400

    added more divs for weather info, queryselected some classes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,11 @@
 // var apiKey = "844421298d794574c100e3409cee0499"
 // var geoCall = `https://api.openweathermap.org/data/2.5/
+
+var searchCityForm = document.querySelector(".searchCityForm");
+var searchCityInput = document.querySelector("#city");
+
+var searchedCityContainer = document.querySelector(".searchedCityContainer");
+var searchedCity = document.querySelector(".searchedCity");
+
+var fiveDayForecastInfo = document.querySelector(".fiveDayForecastInfo");
+var fiveDayForecastInfo = document.querySelector(".fiveDayForecastInfo");
